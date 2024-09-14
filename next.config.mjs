40998d59f62909152fefe27968617ae9c9fa56b0
Daniyal Ahmad <daniyalahmad2535@gmail.com>
2024-09-14 20:27:34 +0500

    Database Fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: ['www.collinsdictionary.com'],
-    },
-  };
-  
-  export default nextConfig;
-  
+  images: {
+    domains: ['www.collinsdictionary.com', 'alkhaleejsupermarket.pk'],
+  },
+};
+
+export default nextConfig;
